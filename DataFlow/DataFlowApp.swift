41b584276617fba958e17c7270d8d91ff1821937
vcosmin2701 <v.cosmin2717@gmail.com>
2024-11-19 08:31:31 +0200

    Initial Commit

---
+++ DataFlow/DataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataFlowApp.swift
+//  DataFlow
+//
+//  Created by Vladut Cosmin on 19.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct DataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
